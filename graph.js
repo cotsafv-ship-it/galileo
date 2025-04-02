2e919d5a561673d8ecacf8f396cdaca79175663a
cotsafv-ship-it <191148503+cotsafv-ship-it@users.noreply.github.com>
2025-04-03 00:52:30 +0800

    Update graph.js
    mixed everything into one

--- graph.js
+++ graph.js
@@ -1,11 +1,4 @@
-function sine(a ,b ,c , y){ //where in the form of y =a sin(bx) +c for a point on the graph
-    let x = 180 * (Math.asin((y -c)/a)/b) / Math.PI //get x in terms of y
-    let data = [x,y] 
-    return data
-}
-
-
-
+//range
 function rangesine(a ,b ,c , limit){ //where in the form of y =a sin(bx) +c and limit of limit of curve
     console.log("for the curve " + a + "sin(" + b + "x) + " + c + ", ")
     let y = null
@@ -35,16 +28,18 @@ function rangecosine(a ,b ,c , limit){ //where in the form of y =a cos(bx) +c an
 
 
 
-function cosine(a ,b ,c , limit){ //where in the form of y =a cos(bx) +c and limit of limit of curve
-    console.log("for the curve " + a + "cos(" + b + "x) + " + c + ", ")
-    let y
-    for (let i = 0; i <= limit; i = i + 10){
-        i = Math.PI * i/180 //torad
-        y = a * Math.cos(b * i) + c
-        console.log("x:" + i + " " + "y:" + y)
-        i = i/Math.PI * 180 //unpi
-    }
-} //need to modify
+//graphical part
+function sine(a ,b ,c , y){ //where in the form of y =a sin(bx) +c for a point on the graph
+    let x = 180 * (Math.asin((y -c)/a)/b) / Math.PI //get x in terms of y
+    let data = [x,y] 
+    return data
+}
+
+function cosine(a ,b ,c , y){ //where in the form of y =a cos(bx) +c and limit of limit of curve
+    let x = 180 * (Math.acos((y -c)/a)/b) / Math.PI //get x in terms of y
+    let data = [x,y] 
+    return data
+}
 
 
 function drawsine(a,b,c){
@@ -53,4 +48,11 @@ function drawsine(a,b,c){
     console.log(current[0],current[1])
 }
 
-drawsine(3,2,1)
+function drawcosine(a,b,c){
+    //put in a loop
+    let current = cosine(a,b,c,3)
+    console.log(current[0],current[1])
+}
+
+//drawsine(3,2,1)
+//drawcosine(3,2,1)
