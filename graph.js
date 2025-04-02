09a8465f9f803c68a36cd8f822c2f6762c8f5ab1
cotsafv-ship-it <191148503+cotsafv-ship-it@users.noreply.github.com>
2025-04-03 00:45:13 +0800

    Update graph.js
    finally made graph.js work

--- graph.js
+++ graph.js
@@ -1,12 +1,7 @@
-function sine(a ,b ,c , limit){ //where in the form of y =a sin(bx) +c and limit of limit of curve
-    console.log("for the curve " + a + "sin(" + b + "x) + " + c + ", ")
-    let y
-    for (let i = 0; i <= limit; i = i + 10){
-        i = Math.PI * i/180 //torad
-        y = a * Math.sin(b * i) + c
-        console.log("x:" + i + " " + "y:" + y)
-        i = i/Math.PI * 180 //unpi
-    }
+function sine(a ,b ,c , y){ //where in the form of y =a sin(bx) +c for a point on the graph
+    let x = 180 * (Math.asin((y -c)/a)/b) / Math.PI //get x in terms of y
+    let data = [x,y] 
+    return data
 }
 
 function cosine(a ,b ,c , limit){ //where in the form of y =a cos(bx) +c and limit of limit of curve
@@ -18,17 +13,13 @@ function cosine(a ,b ,c , limit){ //where in the form of y =a cos(bx) +c and lim
         console.log("x:" + i + " " + "y:" + y)
         i = i/Math.PI * 180 //unpi
     }
+} //need to modify
+
+
+function drawsine(a,b,c){
+    //put in a loop
+    let current = sine(a,b,c,3)
+    console.log(current[0],current[1])
 }
 
-
-
-
-//test values
-//sine(1,2,3,360)
-//cosine(1,2,3,360)
-
-//2a is height of curve in asin(bx) +c
-
-let space = "."
-
-console.log(space.repeat(9) + "O")
+drawsine(3,2,1)
